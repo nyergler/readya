9b83d2ee98cb46919d9289bf831b0c9b5bbf8b72
Nathan Yergler <nathan@yergler.net>
2016-06-02 17:30:39 -0700

    Default to HTTPS

--- publishconf.py
+++ publishconf.py
@@ -11,7 +11,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 SITEDOMAIN = 'www.readya.org'
-SITEURL = 'http://%s' % (SITEDOMAIN,)
+SITEURL = 'https://%s' % (SITEDOMAIN,)
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
